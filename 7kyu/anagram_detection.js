927effe6db265c0b5f0c50debf828a4629f81914
melyaua <igor.melnychenko@tmx-learning.com>
2020-10-13 16:20:26 +0300

    feat: complete two task

---
+++ 7kyu/anagram_detection.js
@@ -0,0 +1,19 @@
+// An anagram is the result of rearranging the letters of a word to produce a new word (see wikipedia).
+
+// Note: anagrams are case insensitive
+
+// Complete the function to return true if the two arguments given are anagrams of each other; return false otherwise.
+
+// Examples
+// "foefet" is an anagram of "toffee"
+
+// "Buckethead" is an anagram of "DeathCubeK"
+
+let isAnagram = function(test, original) {
+  let arr1 = test.toLowerCase().split('').sort().join('');
+  let arr2 = original.toLowerCase().split('').sort().join('');
+  if (arr1 !== arr2) {
+    return false
+  }
+  return true;
+};
